01423fc03d8a4f8d9c071675d612cb15e4498ab4
Andrey Popp <8mayday@gmail.com>
2013-10-09 00:52:55 +0400

    gardening

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 "use strict";
 
-var api = require('./api');
+var path            = require('path'),
+    callsite        = require('callsite'),
+    express         = require('express'),
+    ui              = require('react-app'),
+    pageCollection  = require('./page-collection');
 
-api({path: './posts', debug: true}).listen(3000);
+function promise(func) {
+  return function(req, res, next) {
+    return func(req, res, next)
+      .then(
+        function(response) { response ? res.send(response) : next(); },
+        next);
+  }
+}
+
+function uriToId(uri) {
+  return uri.replace(/\.json$/, '').replace(/\/index$/, '/');
+}
+
+module.exports = function(opts) {
+  var root = opts.root || path.dirname(callsite()[1].getFileName()),
+      app = express(),
+      pages = pageCollection(opts.pages, {watch: opts.debug});
+
+  app.get('*.json', promise(function(req) {
+    var id = uriToId(req.params[0]);
+    return pages.get(id);
+  }));
+
+  app.use(ui(opts.routes, {debug: opts.debug, root: root}));
+
+  return app;
+}
